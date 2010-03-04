1cbc4bc1076e7c186a0cca9bfc0cc3b56608d70b
Sam Tygier <sam.tygier@manchester.ac.uk>
2010-03-04 16:05:14 +0000

    some scripts for decyphering the output files

---
+++ devtools/make_outputs.py
@@ -0,0 +1,56 @@
+
+# makes zgoubi output files fai and plt in ascii and binary
+# note that for plt files, the ascii/binary choise must be made at compile time by modifying include/FILPLT.H
+
+for binary in [True, False]:
+	make_line('line')
+	mass = PROTON_MASS
+	energy = 1e6
+
+
+	b_orig = Bunch()
+	b_orig.gen_halo_x_xp_y_yp(1e4, 1e-3, 1e-3, 4,5,1e-3, 2e-2, seed=4)
+
+	b_orig_4d = numpy.column_stack([b_orig.particles()[col] for col in 'YTZP'])
+
+
+	ob = OBJET2()
+	ob.set(BORO=-ke_to_rigidity(energy, mass))
+	for p in b_orig.particles():
+		ob.add(D=1, Y=p['Y']*100, T=p['T']*1000, Z=p['Z']*100, P=p['P']*1000, LET='A')
+	add(ob)
+	add(PROTON())
+
+	q1 = QUADRUPO(XL=1e-6, XPAS=(1,1,1), IL=2, B_0=1e-50, R_0=1)
+	add(q1)
+
+	if binary:
+		add(FAISCNL(FNAME='b_zgoubi.fai'))
+	else:
+		add(FAISCNL(FNAME='zgoubi.fai'))
+
+	add(END())
+
+	res = run(xterm=False)
+	
+	if binary:
+		try:
+			res.save_b_fai("binary.fai")
+		except IOError:
+			print "could not save binary.fai"
+		try:
+			res.save_b_plt("binary.plt")
+		except IOError:
+			print "could not save binary.plt"
+	else:
+		res.save_fai("ascii.fai")
+		try:
+			res.save_fai("ascii.fai")
+		except IOError:
+			print "could not save ascii.fai"
+		try:
+			res.save_plt("ascii.plt")
+		except IOError:
+			print "could not save ascii.plt"
+
+
---
+++ devtools/read_outputs.py
@@ -0,0 +1,48 @@
+#!/usr/bib/env python
+#import io2 as io
+
+from zgoubi import io
+
+#io.store_def_all() ; exit()
+
+af =  io.read_file("ascii.fai")
+#print af
+print af.shape
+print af[0]
+
+bf =  io.read_file("binary.fai")
+#print bf
+print bf.shape
+print bf[0]
+
+#for n, p in zip( af[0], bf[0]): print p
+
+
+
+print "plt"
+
+af =  io.read_file("ascii.plt")
+#print af
+print af.shape
+print af[0]
+
+bf =  io.read_file("binary.plt")
+#print bf
+print bf.shape
+print bf[0]
+
+#for p in zip( af[0], bf[0]): print p
+
+
+
+
+
+import timeit
+
+#print timeit.timeit('io.define_file("ascii.fai")', setup="import io2 as io", number=10)
+#print timeit.timeit('io.define_file("ascii.plt")', setup="import io2 as io", number=10)
+#print timeit.timeit('io.define_file("ascii.fai", allow_lookup=False)', setup="import io2 as io", number=100)
+
+#print timeit.timeit('io.read_file("ascii.fai")', setup="import io2 as io", number=100)
+#print timeit.timeit('io.read_file("binary.fai")', setup="import io2 as io", number=100)
+
