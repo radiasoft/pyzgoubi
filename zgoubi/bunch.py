d844ee42bdf1d4fe7c28533436dc6957640a1cb3
Sam Tygier <sam.tygier@manchester.ac.uk>
2010-03-10 15:17:23 +0000

    Some pylint clean up in bunch

--- zgoubi/bunch.py
+++ zgoubi/bunch.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
+
+"A bunch object to hold the coordinates for many particles"
+
 from math import *
 import numpy
 import pylab
-from zgoubi import rel_conv
-import os
+#from zgoubi import rel_conv
 
-from zgoubi.utils import *
+#from zgoubi.utils import *
 
 
 class Bunch(object):
-	"""Object to store a bunch of particles efficently using numpy.
+	"""Object to store a bunch of particles efficiently using numpy.
 	All values are in SI units, m, rad, eV, s
 
 	"""
@@ -22,39 +24,31 @@ class Bunch(object):
 	('tof', numpy.float64),
 	('S', numpy.float64),
 	('X', numpy.float64),
-	('keep', numpy.bool)
 	]
 	def __init__(self, nparticles=0, ke=0, mass=0):
 		self.coords = numpy.zeros(nparticles, self.data_def)
 		self.coords['KE'] = ke
-		self.coords['keep'] = True
 		self.mass = mass
 
 	def particles(self):
+		"Returns the numpy array that holds the coordinates"
 		return self.coords
 
-	def get_min_BORO(self, mass):
-		min_BORO = ke_to_rigidity(min(self.coords['KE']), mass)
+	def get_min_BORO(self):
+		"Returns the minimum rigidy of the bunch"
+		min_BORO = ke_to_rigidity(min(self.coords['KE']), self.mass)
 		return min_BORO
 
-	def drop_unkept(self):
-		new_count = self.coords['keep'].sum()
-		new_coords = numpy.zeros(new_count, self.data_def)
-		i = 0
-		for p in self.coords:
-			if p['keep']:
-				new_coords[i] = p
-				i += 1
-		self.coords = new_coords
 
 
 	def gen_halo_x_xp_y_yp(self, npart, emit_y, emit_z, beta_y, beta_z, alpha_y, alpha_z, seed=None):
+		"Generate a halo bunch, i.e. an elipse in x-xp (Y-T) and in y-yp (Z-P)"
 
 		#r = numpy.random.random_sample([npart])
 		ry = sqrt(emit_y) 
 		rz = sqrt(emit_z) 
 
-		if seed!=None:
+		if seed != None:
 			numpy.random.seed(seed)
 
 		u1 = numpy.random.random_sample([npart]) * pi * 2
@@ -65,24 +59,24 @@ class Bunch(object):
 		coords = numpy.zeros([npart, 6], numpy.float64)
 		coords2 = numpy.zeros([npart, 6], numpy.float64)
 
-		coords[:,0] = ry * numpy.cos(u1)
-		coords[:,1] = ry * numpy.sin(u1)
-		coords[:,2] = rz * numpy.cos(u2)
-		coords[:,3] = rz * numpy.sin(u2)
+		coords[:, 0] = ry * numpy.cos(u1)
+		coords[:, 1] = ry * numpy.sin(u1)
+		coords[:, 2] = rz * numpy.cos(u2)
+		coords[:, 3] = rz * numpy.sin(u2)
 
-		matrix = self._twiss_matrix(beta_y, beta_z, alpha_y, alpha_z )
+		matrix = self._twiss_matrix(beta_y, beta_z, alpha_y, alpha_z)
 		
-		for n,coord in enumerate(coords):
+		for n, coord in enumerate(coords):
 			#	new_coord = numpy.dot(coord, matrix)
 			new_coord = numpy.dot(matrix, coord)
 			coords2[n] = new_coord 
 		
-		bunch =  numpy.zeros([npart],self.data_def)
+		bunch =  numpy.zeros([npart], self.data_def)
 		
-		bunch['Y'] = coords2[:,0]
-		bunch['T'] = coords2[:,1]
-		bunch['Z'] = coords2[:,2]
-		bunch['P'] = coords2[:,3]
+		bunch['Y'] = coords2[:, 0]
+		bunch['T'] = coords2[:, 1]
+		bunch['Z'] = coords2[:, 2]
+		bunch['P'] = coords2[:, 3]
 		
 	#	bunch['Y'] = coords[:,0]
 	#	bunch['T'] = coords[:,1]
@@ -90,63 +84,75 @@ class Bunch(object):
 	#	bunch['P'] = coords[:,3]
 		self.coords = bunch
 
-	def _twiss_matrix(self, beta_y, beta_z, alpha_y, alpha_z ):
+	def _twiss_matrix(self, beta_y, beta_z, alpha_y, alpha_z):
+		"Create a matrix that will convert a spherical distribution in to one with the correct twiss values"
 		B = numpy.eye(6)
-		B[0,0] = sqrt(beta_y)
-		B[1,1] = 1/sqrt(beta_y)
-		B[2,2] = sqrt(beta_z)
-		B[3,3] = 1/sqrt(beta_z)
+		B[0, 0] = sqrt(beta_y)
+		B[1, 1] = 1 / sqrt(beta_y)
+		B[2, 2] = sqrt(beta_z)
+		B[3, 3] = 1 / sqrt(beta_z)
 
 		A = numpy.eye(6)
-		A[1,0] = -alpha_y
-		A[3,2] = -alpha_z
+		A[1, 0] = -alpha_y
+		A[3, 2] = -alpha_z
 
 		#M = numpy.dot(A, B)
 		M = numpy.dot(B, A)
 		return M
 
 
-	def read_YTZPSD(self, fname, ke):
+	def read_YTZPSD(self, fname, ke, lim=None):
+		"Read in a bunch from a file. assumes columns are Y, T, Z, P, X, D (though D is ignored), separated by white space"
 		dist = numpy.loadtxt(fname)
 		nparts = dist.size / 6
-		dist = dist.reshape(nparts,6)
+		dist = dist.reshape(nparts, 6)
+		if lim:
+			dist = dist[:lim]
 		self.coords = numpy.zeros(nparts, self.data_def)
-		self.coords['keep'] = True
+		# FIXME should set KE from D
 		self.coords['KE'] = ke
-		self.coords['Y'] = dist[:,0]
-		self.coords['T'] = dist[:,1]
-		self.coords['Z'] = dist[:,2]
-		self.coords['P'] = dist[:,3]
-		self.coords['X'] = dist[:,4]
-		self.coords['D'] = dist[:,5]
+		self.coords['Y'] = dist[:, 0]
+		self.coords['T'] = dist[:, 1]
+		self.coords['Z'] = dist[:, 2]
+		self.coords['P'] = dist[:, 3]
+		self.coords['X'] = dist[:, 4]
+		#self.coords['D'] = dist[:,5]
 
 	def write_YTZPSD(self, fname):
+		"Output a bunch, compatible with read_YTZPSD"
 		nparts = len(self.coords)
 		dist = numpy.zeros([nparts, 6])
-		dist[:,0] = self.coords['Y']
-		dist[:,1] = self.coords['T']
-		dist[:,2] = self.coords['Z']
-		dist[:,3] = self.coords['P']
-		dist[:,4] = self.coords['X']
-		dist[:,5] = self.coords['D']
-		dist = dist.reshape(nparts*2,3)
+		#FIXME should set D from KE
+		dist[:, 0] = self.coords['Y']
+		dist[:, 1] = self.coords['T']
+		dist[:, 2] = self.coords['Z']
+		dist[:, 3] = self.coords['P']
+		dist[:, 4] = self.coords['X']
+		#dist[:,5] = self.coords['D']
+		dist[:, 5] = 1
+		dist = dist.reshape(nparts * 2, 3)
 		numpy.savetxt(fname, dist)
 
 	
 	def get_widths(self):
+		"Returns the width of the bunch in each dimension Y,T,Z,P,X,D (D not calculated yet)"
 		y_width = max(self.coords['Y']) - min(self.coords['Y'])
 		t_width = max(self.coords['T']) - min(self.coords['T'])
 		z_width = max(self.coords['Z']) - min(self.coords['Z'])
 		p_width = max(self.coords['P']) - min(self.coords['P'])
 		x_width = max(self.coords['X']) - min(self.coords['X'])
-		d_width = max(self.coords['D']) - min(self.coords['D'])
+		#d_width = max(self.coords['D']) - min(self.coords['D'])
+		# FIXME should set from KE
+		d_width = 0
 		return (y_width, t_width, z_width, p_width, x_width, d_width)
 
 	def __len__(self):
+		"Returns length of bunch. Use len(my_bunch)"
 		return len(self.coords)
 
 	def plot(self, fname=None, lims=None):
-		pylab.subplot(2,2,1)
+		"Plot a bunch, if no file name give plot is displayed on screen."
+		pylab.subplot(2, 2, 1)
 		pylab.grid()
 		pylab.plot(self.coords['Y'], self.coords['Z'], ',')
 		if lims != None:
@@ -156,7 +162,7 @@ class Bunch(object):
 		pylab.title(plotname)
 
 		
-		pylab.subplot(2,2,2)
+		pylab.subplot(2, 2, 2)
 		pylab.grid()
 		pylab.plot(self.coords['Y'], self.coords['T'], ',')
 		if lims != None:
@@ -165,7 +171,7 @@ class Bunch(object):
 		plotname = "X-XP (Y-T"
 		pylab.title(plotname)
 
-		pylab.subplot(2,2,3)
+		pylab.subplot(2, 2, 3)
 		pylab.grid()
 		pylab.plot(self.coords['Z'], self.coords['P'], ',')
 		if lims != None:
@@ -174,7 +180,7 @@ class Bunch(object):
 		plotname = "Y-YP (z-P)"
 		pylab.title(plotname)
 
-		pylab.subplot(2,2,4)
+		pylab.subplot(2, 2, 4)
 		pylab.grid()
 		pylab.plot(self.coords['X'], self.coords['D'], ',')
 		if False:# lims != None:
@@ -204,32 +210,36 @@ class Bunch(object):
 		theta_zp = theta_zp - major_angle
 		rot_z = r_zp * numpy.sin(theta_zp)
 		rot_p = r_zp * numpy.cos(theta_zp)
-		emmitance_v = rot_z.max()*rot_p.max()
+		emmitance_v = rot_z.max() * rot_p.max()
 		#print "Emmitance (h, v):", emmitance_h, emmitance_v
 		return (emmitance_h, emmitance_v)
 
 
 	def get_twiss(self, emittance):
+		"Returns the twiss valuse Beta_h, Alpha_h, Beta_v, Alpha_v, calculated from bunch width extent"
 		# emittance may be a single number, or tuple (emittance_h, emittance_v)
 		try:
 			emittance_h, emittance_v = emittance
 		except TypeError:
 			emittance_h = emittance_v = emittance
 		widths = self.get_widths()
-		beta_h = (widths[0]/2)**2 / emittance_h
-		beta_v = (widths[2]/2)**2 / emittance_v
+		beta_h = (widths[0] / 2)**2 / emittance_h
+		beta_v = (widths[2] / 2)**2 / emittance_v
 		#print "beta", beta_h, beta_v
-		gamma_h = (widths[1]/2)**2 / emittance_h
-		gamma_v = (widths[3]/2)**2 / emittance_v
+		#gamma_h = (widths[1] / 2)**2 / emittance_h
+		#gamma_v = (widths[3] / 2)**2 / emittance_v
 		# get T of particle with bigest Y
+		# FIXME, maybe can also look at T of particle with smallest Y, and average
 		y_p = self.coords['T'][self.coords['Y'].argmax()]
-		alpha_h = - y_p / sqrt(emittance_h/beta_h)
+		alpha_h = - y_p / sqrt(emittance_h / beta_h)
 		z_p = self.coords['P'][self.coords['Z'].argmax()]
-		alpha_v = - z_p / sqrt(emittance_v/beta_v)
+		alpha_v = - z_p / sqrt(emittance_v / beta_v)
 		#print "gamma", gamma_h, gamma_v	
+
 		# following is dangerous, due do stat fluctuations causing roots of negative numbers
-		#alpha_h = sqrt((gamma_h*beta_h)-1 )
-		#alpha_v = sqrt((gamma_v*beta_v)-1 )
+		#	alpha_h = sqrt((gamma_h*beta_h)-1 )
+		#	alpha_v = sqrt((gamma_v*beta_v)-1 )
+
 		#print "twiss (bh,ah,bv,av):", beta_h, alpha_h, beta_v, alpha_v
 		return beta_h, alpha_h, beta_v, alpha_v
 
